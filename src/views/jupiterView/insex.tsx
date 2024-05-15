4355fe42b5919db0eedf818d43936185b0f3be26
96ayad <167616524+96ayad@users.noreply.github.com>
2024-05-15 12:16:40 +0300

    jupiter view

---
+++ src/views/jupiterView/insex.tsx
@@ -0,0 +1,73 @@
+'use client'
+import { useEffect } from 'react';
+import styles from './index.module.css';
+
+import Link from "next/link";
+import { FC } from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+import { SolanaLogo } from "components";
+import styles from "./index.module.css";
+
+export const HomeView: FC = ({}) => {
+  const { publicKey } = useWallet();
+
+  const onClick = () => {};
+
+  useEffect(() => {
+    // Dynamically load the Jupiter script
+    const script = document.createElement('script');
+    script.src = "https://terminal.jup.ag/main-v2.js";
+    script.onload = () => launchJupiter(); // Initialize Jupiter after the script loads
+    document.head.appendChild(script);
+  }, []);
+
+  function launchJupiter() {
+    if (window.Jupiter) {
+      window.Jupiter.init({ 
+        displayMode: "integrated",
+        integratedTargetId: "integrated-terminal",
+        endpoint: "https://mainnet.helius-rpc.com/?api-key=YOUR_API_KEY_HERE",
+        strictTokenList: false,
+        defaultExplorer: "SolanaFM",
+        formProps: {
+          initialAmount: "888888880000",
+          initialInputMint: "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263",
+          initialOutputMint: "AZsHEMXd36Bj1EMNXhowJajpUXzrKcK57wW4ZGXVa7yR",
+        },
+      });
+    } else {
+      console.error("Jupiter script not loaded yet");
+    }
+  }
+
+  return (
+    <div className="container mx-auto max-w-6xl p-8 2xl:px-0">
+      <div className={styles.container}>
+        <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
+          <div className="flex-none">
+            <button className="btn btn-square btn-ghost">
+              <span className="text-4xl">🦤</span>
+            </button>
+          </div>
+          <div className="flex-1 px-2 mx-2">
+            <span className="text-lg font-bold">Caw Caw</span>
+          </div>
+          <div className="flex-none">
+            <WalletMultiButton className="btn btn-ghost" />
+          </div>
+        </div>
+
+
+        <div className={styles.body}>
+ 
+        <div id="integrated-terminal"></div>
+
+        </div>
+        
+
+      </div>
+    </div>
+  );
+};
